a24f9c00229f754e34436583c139b1be1828d748
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2018-03-30 09:04:29 +0200

    Setup frontend router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
 
+import history from './modules/history';
 import App from './containers/appContainer/app-container';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
